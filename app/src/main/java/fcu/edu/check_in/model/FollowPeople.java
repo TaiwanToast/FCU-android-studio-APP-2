cf947816c7d17f82938bbef9dd81d5c9f7cde268
TaiwanToast <132571695+TaiwanToast@users.noreply.github.com>
2025-05-19 23:42:45 +0800

    改善Person model，一個人的最小駔成單位

--- app/src/main/java/fcu/edu/check_in/model/FollowPeople.java
+++ app/src/main/java/fcu/edu/check_in/model/FollowPeople.java
@@ -1,5 +1,6 @@
 package fcu.edu.check_in.model;
 
+// 可以用Person取代
 public class FollowPeople {
     private final String followName;
     public FollowPeople(String followName) {
--- app/src/main/java/fcu/edu/check_in/model/Person.java
+++ app/src/main/java/fcu/edu/check_in/model/Person.java
@@ -1,8 +1,29 @@
 package fcu.edu.check_in.model;
 
+import java.util.List;
+
 public class Person {
-    private String name;
-    public Person(String name){
-        this.name = name;
+    private String nickName, bio;
+    private final String email;
+
+    private List<String> followPersonEmail;
+
+    private List<MyTask> myTasks;
+
+    public Person(String nickName, String bio, String email, List<String> followPersonEmail, List<MyTask> myTasks){
+        this.nickName = nickName;
+        this.bio = bio;
+        this.email = email;
+        this.followPersonEmail = followPersonEmail;
+        this.myTasks = myTasks;
     }
+
+    public String getNickName(){ return this.nickName;};
+    public String getBio() { return bio; }
+    public String getEmail() {return email;}
+    public void setBio(String bio) { this.bio = bio; }
+    public void setNickName(String nickName) { this.nickName = nickName; }
+
+    public void addFollow(String followEmail){ this.followPersonEmail.add(followEmail); }
+    public void addTask(MyTask newTask){ this.myTasks.add(newTask); }
 }
